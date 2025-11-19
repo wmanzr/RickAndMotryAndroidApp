d458ab4c3c5d5edb910408023ecec8cf094a1850
wmanzr <eablofen@list.ru>
2025-11-19 21:06:35 +0300

    update resources structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
